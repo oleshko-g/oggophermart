a20478f5669dd58c81d88a62cd09a5ecaa6ef2dd
Gennady Oleshko <gennady@oleshko-g.ru>
2026-01-02 19:46:12 +0500

    refactor errorHandler
    Signed-off-by: Gennady Oleshko <gennady@oleshko-g.ru>

--- internal/transport/http/server.go
+++ internal/transport/http/server.go
@@ -28,20 +28,21 @@ type server struct {
 
 func newMux(balanceEndpoints *balance.Endpoints, userEndpoints *user.Endpoints) goahttp.Muxer {
 	var (
-		reqDecoder   func(r *http.Request) goahttp.Decoder
-		resEncoder   func(ctx context.Context, res http.ResponseWriter) goahttp.Encoder
-		mux          goahttp.Muxer
-		errorHandler func(context.Context, http.ResponseWriter, error)
+		reqDecoder func(r *http.Request) goahttp.Decoder
+		resEncoder func(ctx context.Context, res http.ResponseWriter) goahttp.Encoder
+		mux        goahttp.Muxer
+		errHandler func(context.Context, http.ResponseWriter, error)
 	)
 	{
 		reqDecoder = goahttp.RequestDecoder
 		resEncoder = goahttp.ResponseEncoder
+		errHandler = errorHandler
 		mux = goahttp.NewMuxer()
 	}
 
 	// create HTTP servers
-	balanceServer := genBalanceHTTPSrv.New(balanceEndpoints, mux, reqDecoder, resEncoder, errorHandler, nil)
-	userServer := genUserHTTPSvr.New(userEndpoints, mux, reqDecoder, resEncoder, errorHandler, nil)
+	balanceServer := genBalanceHTTPSrv.New(balanceEndpoints, mux, reqDecoder, resEncoder, errHandler, nil)
+	userServer := genUserHTTPSvr.New(userEndpoints, mux, reqDecoder, resEncoder, errHandler, nil)
 
 	// mount HTTP endpoint onto mux
 	balanceServer.Mount(mux)
@@ -72,27 +73,25 @@ func NewServer(cfg Config, svc service.Service) Server {
 }
 
 // errorHandler is the handler which is called when ther was an HTTP response encoding error
-func errorHandler() func(context.Context, http.ResponseWriter, error) {
-	return func(ctx context.Context, res http.ResponseWriter, err error) {
-		if res == nil {
-			err = fmt.Errorf("%w: %s", errResponseWithError, errors.New("nil responseWriter"))
-			slog.Error(err.Error())
-			return
-		}
-		res.Header().Set("Content-Type", "text/plain")
-
-		var statusCode int
-		if err == nil {
-			// replace the err and HTTP status code and still write the response
-			err = fmt.Errorf("%w: %s", errResponseWithError, errors.New("nil err"))
-			statusCode := http.StatusInternalServerError
-			slog.Error(err.Error())
-			http.Error(res, err.Error(), statusCode)
-			return
-		}
-
-		http.Error(res, err.Error(), statusCode)
+func errorHandler(ctx context.Context, res http.ResponseWriter, err error) {
+	if res == nil {
+		err = fmt.Errorf("%w: %s", errResponseWithError, errors.New("nil responseWriter"))
+		slog.Error(err.Error())
+		return
 	}
+	res.Header().Set("Content-Type", "text/plain")
+
+	var statusCode int
+	if err == nil {
+		// replace the err and HTTP status code and still write the response
+		err = fmt.Errorf("%w: %s", errResponseWithError, errors.New("nil err"))
+		statusCode := http.StatusInternalServerError
+		slog.Error(err.Error())
+		http.Error(res, err.Error(), statusCode)
+		return
+	}
+
+	http.Error(res, err.Error(), statusCode)
 }
 
 var errResponseWithError = errors.New("failed to response with error")
